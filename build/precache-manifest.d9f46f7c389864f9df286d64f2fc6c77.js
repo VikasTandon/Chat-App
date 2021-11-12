a1d5c08ab595c160810e76c30738ee0d6e953a7a
Vikas Tandon <vikastandon56@gmail.com>
2021-11-12 22:50:50 +0530

    Chat App

---
+++ build/precache-manifest.d9f46f7c389864f9df286d64f2fc6c77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9d32605a686ca2689b3ea9148520c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57bcbeb2e7b33a88f5b1",
+    "url": "/static/css/main.085538cd.chunk.css"
+  },
+  {
+    "revision": "486bf568686eb5b8a48f",
+    "url": "/static/js/2.948e4252.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.948e4252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57bcbeb2e7b33a88f5b1",
+    "url": "/static/js/main.9af19c42.chunk.js"
+  },
+  {
+    "revision": "eb528f885b225b7fcf37",
+    "url": "/static/js/runtime-main.8900c460.js"
+  }
+]);
